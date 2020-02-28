5689a66e5c3e606c99c853536ca5a2d7f5f1d515
weizd21 <17554157104@163.com>
2020-02-28 19:28:51 +0800

    维护redis-starter模块

---
+++ oldwei-base-starter/oldwei-redis-starter/src/main/java/top/oldwei/redis/constant/RedisConstant.java
@@ -0,0 +1,30 @@
+package top.oldwei.redis.constant;
+
+/**
+ * @Author:weizd
+ * @Date:20-2-27
+ *
+ * redis 类型
+ */
+public interface RedisConstant {
+
+    /**
+     * 哨兵
+     */
+    String REDIS_SENTINEL = "sentinel";
+
+    /**
+     * 集群
+     */
+    String REDIS_CLUSTER = "cluster";
+
+    /**
+     * 单机
+     */
+    String REDIS_SINGLE = "single";
+
+
+
+
+
+}
