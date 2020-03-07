9147232e4041fd886620e5fde3f3148a58a7ba01
weizd21 <17554157104@163.com>
2020-03-07 11:34:24 +0800

    修改redis配置文件sentinel哨兵方式

--- oldwei-user/src/main/java/top/oldwei/user/UserApplication.java
+++ oldwei-user/src/main/java/top/oldwei/user/UserApplication.java
@@ -5,6 +5,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -21,6 +23,8 @@ import java.net.UnknownHostException;
 @EnableTransactionManagement
 @Slf4j
 @MapperScan(value = "top.oldwei.**.mapper")
+@EnableDiscoveryClient
+@EnableConfigurationProperties
 public class UserApplication {
 
 
