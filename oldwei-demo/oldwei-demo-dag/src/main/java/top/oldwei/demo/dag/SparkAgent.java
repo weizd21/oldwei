fd302c753b16881092a08cc19a066327e908a97a
weizd <weizd@dataexa.com>
2020-09-28 19:50:40 +0800

    视频处理demo

--- oldwei-demo/oldwei-demo-dag/src/main/java/top/oldwei/demo/dag/SparkAgent.java
+++ oldwei-demo/oldwei-demo-dag/src/main/java/top/oldwei/demo/dag/SparkAgent.java
@@ -1,12 +1,15 @@
 package top.oldwei.demo.dag;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.db.Db;
 import cn.hutool.db.Entity;
+import cn.hutool.db.ds.simple.SimpleDataSource;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import top.oldwei.demo.dag.domain.Dag;
 import top.oldwei.demo.dag.domain.Vertice;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,15 +23,24 @@ public class SparkAgent {
     public static void main(String[] args) throws SQLException {
         log.info("spark agent");
 
-        List<Entity> list = Db.use().findAll("d_dag_flow");
+        String url = "jdbc:mysql://localhost:3306/dataexa";
+        String user = "root";
+        String password = "123456";
+//        String driver = "";
+        DataSource dataSource = new SimpleDataSource(url,user,password);
+
+        List<Entity> list = Db.use(dataSource).findAll("d_dag_flow");
         log.info("size: {}",list.size());
         List<Dag> dags = new ArrayList<>();
         Dag dag = null;
+        String dagStr = "";
         for(Entity entity:list){
             log.info(entity.getStr("dag_json"));
-
-            dag = JSONObject.parseObject(entity.get("dag_json").toString(),Dag.class);
-            dags.add(dag);
+            dagStr = entity.getStr("dag_json");
+            if(!StrUtil.isEmpty(dagStr)){
+                dag = JSONObject.parseObject(dagStr,Dag.class);
+                dags.add(dag);
+            }
         }
 
         for (Dag d: dags){
