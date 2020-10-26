8d15ef9c73c92b467ab476f65f1e9bf40c3a897d
zxm <zxm@192.168.0.104>
2020-10-27 00:00:48 +0800

    dag测试

--- oldwei-demo/oldwei-demo-dag/src/main/java/top/oldwei/demo/dag/SparkAgent.java
+++ oldwei-demo/oldwei-demo-dag/src/main/java/top/oldwei/demo/dag/SparkAgent.java
@@ -7,6 +7,7 @@ import cn.hutool.db.ds.simple.SimpleDataSource;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import top.oldwei.demo.dag.domain.Dag;
+import top.oldwei.demo.dag.domain.Edge;
 import top.oldwei.demo.dag.domain.Vertice;
 
 import javax.sql.DataSource;
@@ -23,7 +24,7 @@ public class SparkAgent {
     public static void main(String[] args) throws SQLException {
         log.info("spark agent");
 
-        String url = "jdbc:mysql://localhost:3306/dataexa";
+        String url = "jdbc:mysql://localhost:3306/insight-58";
         String user = "root";
         String password = "123456";
 //        String driver = "";
@@ -34,19 +35,49 @@ public class SparkAgent {
         List<Dag> dags = new ArrayList<>();
         Dag dag = null;
         String dagStr = "";
+
         for(Entity entity:list){
-            log.info(entity.getStr("dag_json"));
+            Long id = entity.getLong("id");
             dagStr = entity.getStr("dag_json");
-            if(!StrUtil.isEmpty(dagStr)){
+            if(!StrUtil.isEmpty(dagStr) && dagStr.contains("110.48242855072021")){
+                log.info("-----------------> {}",id);
                 dag = JSONObject.parseObject(dagStr,Dag.class);
                 dags.add(dag);
             }
         }
+        // 正确的dag流程ID 10217
 
-        for (Dag d: dags){
-            log.info("start vertice: {}", d.getVerticeByType("start"));
+        for(Edge edge:dag.getEdges()){
+            log.info("start :[{}] ,end :[{}]",edge.getStart(),edge.getEnd());
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        for(Entity entity:list){
+//            log.info(entity.getStr("dag_json"));
+//            dagStr = entity.getStr("dag_json");
+//            if(!StrUtil.isEmpty(dagStr)){
+//                dag = JSONObject.parseObject(dagStr,Dag.class);
+//                dags.add(dag);
+//            }
+//        }
+//
+//        for (Dag d: dags){
+//            log.info("start vertice: {}", d.getVerticeByType("start"));
+//        }
+//
 
     }
 }
